aa1dc584880dc7cb76b4f9a4045e55ec8307629a
cristian <20201032@uaz.edu.mx>
2024-11-04 22:36:07 -0600

    Node.js Fundamentals

---
+++ hello-world-node/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array);
+
+setTimeout(() => {
+    console.log("This message is delayed by 2 seconds.");
+}, 2000);
+
+console.log("This message will be printed after the delay.");
+
+
